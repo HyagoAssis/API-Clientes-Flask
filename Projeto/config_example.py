9d77d64cfbfa2f34d8455a06274f4b45321eac26
Hyago <hyagoassis@ice.ufjf.br>
2021-09-10 20:36:29 -0300

    Alterando gitignore

---
+++ Projeto/config_example.py
@@ -0,0 +1,9 @@
+config = {}
+config["DB_CONNECTION"]="postgresql"
+config["DB_HOST"]="localhost"
+config["DB_PORT"]="3306"
+config["DB_DATABASE"]="prova"
+config["DB_USERNAME"]="postgres"
+config["DB_PASSWORD"]=""
+
+
