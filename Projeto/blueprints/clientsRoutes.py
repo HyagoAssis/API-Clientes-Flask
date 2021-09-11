1e1e2c4a59ac9b79313ebce5473f7d88b3500a14
Hyago <hyagoassis@ice.ufjf.br>
2021-09-10 22:21:24 -0300

    Adicionando README.md

--- Projeto/blueprints/clientsRoutes.py
+++ Projeto/blueprints/clientsRoutes.py
@@ -10,18 +10,9 @@ def selectAllClients():
     clientsList = Clients.query.all()
     clientsJson = [client.to_json() for client in clientsList]
 
-    return gera_response(200, clientsJson, "ok", "null")
+    return generateResponse(200, clientsJson, "ok", "null")
 
-
-@api.route("/client/<id>", methods=["GET"])
-def selectClient(id):
-    client = Clients.query.filter_by(id=id).first()
-    clientJson = client.to_json()
-
-    return gera_response(200, clientJson, "ok", "null")
-
-
-@api.route("/client", methods=["POST"])
+@api.route("/client/create", methods=["POST"])
 def createClient():
     body = request.get_json()
 
@@ -31,13 +22,13 @@ def createClient():
         print(client)
         db.session.add(client)
         db.session.commit()
-        return gera_response(201, client.to_json(), "Criado com sucesso", "null")
+        return generateResponse(200, client.to_json(), "Criado com sucesso", "null")
     except Exception as e:
         print(e)
-        return gera_response(400, {}, "Erro ao cadastrar", "Error")
+        return generateResponse(400, {}, "Erro ao cadastrar", "Error")
 
 
-@api.route("/client/<id>", methods=["PUT"])
+@api.route("/client/update/e<id>", methods=["PUT"])
 def updateClient(id):
     client = Clients.query.filter_by(id=id).first()
     body = request.get_json()
@@ -54,27 +45,27 @@ def updateClient(id):
 
         db.session.add(client)
         db.session.commit()
-        return gera_response(201, client.to_json(), "Atualizado com sucesso", "null")
+        return generateResponse(200, client.to_json(), "Atualizado com sucesso", "null")
 
     except Exception as e:
         print(e)
-        return gera_response(400, {}, "Erro ao atualzar", "Error")
+        return generateResponse(400, {}, "Erro ao atualzar", "Error")
 
 
-@api.route("/client/<id>", methods=["DELETE"])
+@api.route("/client/delete/<id>", methods=["DELETE"])
 def deleteClient(id):
     client = Clients.query.filter_by(id=id).first()
 
     try:
         db.session.delete(client)
         db.session.commit()
-        return gera_response(201, {}, "Apagado com sucesso", "null")
+        return generateResponse(200, {}, "Apagado com sucesso", "null")
     except Exception as e:
         print(e)
-        return gera_response(400, {}, "Erro ao deletar", "Error")
+        return generateResponse(400, {}, "Erro ao deletar", "Error")
 
 
-def gera_response(status, content, mensagem, error):
+def generateResponse(status, content, mensagem, error):
     body = {}
     body["status"] = status
     body["Clients"] = content
